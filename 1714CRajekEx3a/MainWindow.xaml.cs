7ccaa38fa477d4fbff33006705d9b1ac5503f7ae
CRajek <cory.rajek@mysoutheastmn.edu>
2020-11-14 11:28:05 -0600

    Completed Ex3a

--- 1714CRajekEx3a/MainWindow.xaml.cs
+++ 1714CRajekEx3a/MainWindow.xaml.cs
@@ -23,6 +23,7 @@ namespace _1714CRajekEx3a
     public partial class MainWindow : Window
     {
         System.Windows.Data.CollectionViewSource buildingViewSource;
+        PropertyManager2ModelContainer dbContext;
         public MainWindow()
         {
             InitializeComponent();
@@ -33,34 +34,35 @@ namespace _1714CRajekEx3a
 
             buildingViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("buildingViewSource")));
             // Load data by setting the CollectionViewSource.Source property:
-            PropertyManager2ModelContainer dbContext = new PropertyManager2ModelContainer();
-            dbContext.Buildings.Load(); 
+            dbContext = new PropertyManager2ModelContainer();
+            dbContext.Buildings.Load();
             buildingViewSource.Source = dbContext.Buildings.Local;
+            dbContext.Configuration.AutoDetectChangesEnabled = true;
         }
 
         private void firstButton_Click(object sender, RoutedEventArgs e)
         {
-            buildingViewsource.View.MoveCurrentToFirst();
+            buildingViewSource.View.MoveCurrentToFirst();
         }
 
         private void backButton_Click(object sender, RoutedEventArgs e)
         {
-            buildingViewsource.View.MoveCurrentToPrevious();
+            buildingViewSource.View.MoveCurrentToPrevious();
         }
 
         private void nextButton_Click(object sender, RoutedEventArgs e)
         {
-            buildingViewsource.View.MoveCurrentToNext();
+            buildingViewSource.View.MoveCurrentToNext();
         }
 
         private void lastButton_Click(object sender, RoutedEventArgs e)
         {
-            buildingViewsource.View.MoveCurrentToLast();
+            buildingViewSource.View.MoveCurrentToLast();
         }
 
         private void saveButton_Click(object sender, RoutedEventArgs e)
         {
-            
+            dbContext.SaveChanges();
         }
     }
 }
