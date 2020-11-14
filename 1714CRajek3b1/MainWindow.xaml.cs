b832e94459e7836230f692e21c13caa56d7abd54
CRajek <cory.rajek@mysoutheastmn.edu>
2020-11-14 16:20:22 -0600

    17143b1 Error

---
+++ 1714CRajek3b1/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using _1714CRajekex2g;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _1714CRajek3b1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PropertyManager2ModelContainer dbContext;
+        System.Windows.Data.CollectionViewSource personViewSource;
+
+        public MainWindow()
+
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            personViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("personViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            dbContext = new PropertyManager2ModelContainer();
+            dbContext.People.Load();
+            personViewSource.Source = dbContext.People.Local;
+            dbContext.Configuration.AutoDetectChangesEnabled = true;
+        }
+
+        private void firstButton_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void backButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void nextButton_Click(object sender, RoutedEventArgs e)
+        {
+            personViewSource.View.MoveCurrentToNext();
+ 
+         }
+ 
+        private void lastButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+       
+    }
+}
